3491cf56e9387bcaa0c2c190652aad82fb49e818
tamkeen99 <119537780+tamkeen99@users.noreply.github.com>
2024-01-26 19:29:49 +0530

    committing to update branch

--- src/main/java/com/SwagLab/pom/SwagLabPage.java
+++ src/main/java/com/SwagLab/pom/SwagLabPage.java
@@ -55,6 +55,44 @@ public class SwagLabPage {
 	}	
 	
 	
+	
+	
+
+	@FindBy(id="add-to-cart-sauce-labs-fleece-jacket")
+	private WebElement add4;
+	
+	
+	public void setAdd4() {
+		add4.click();
+	}	
+	
+	
+	
+	
+	
+
+	@FindBy(id="add-to-cart-sauce-labs-onesie")
+	private WebElement add5 ;
+	
+	
+	public void setAdd5() {
+		add5.click();
+	}	
+	
+	
+	@FindBy(id="add-to-cart-test.allthethings()-t-shirt-(red)")
+	private WebElement add6 ;
+	
+	
+	public void setAdd6() {
+		add6.click();
+	}	
+	
+	
+	
+	
+	
+	
 	@FindBy(className="shopping_cart_link")
 	private WebElement cart ;
 	

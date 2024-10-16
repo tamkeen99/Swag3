caef2a2ff43753ba85d70b8fa78d7c2c167fa158
tamkeen99 <tamkeenmks456@gmail.com>
2024-10-16 16:29:03 +0530

    this is first commit

--- src/main/java/com/SwagLab/generic/BaseClass.java
+++ src/main/java/com/SwagLab/generic/BaseClass.java
@@ -6,15 +6,11 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-
 import com.SwagLab.pom.LogOutPage;
 import com.SwagLab.pom.LoginPage;
 
